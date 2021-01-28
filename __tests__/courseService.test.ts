17de9a008a2b40187437cf901b9e49a7bbf3155b
SIY1121 <sota.0512.0213@gmail.com>
2021-01-28 08:49:34 +0000

    refactor: courseServiceTest

--- __tests__/courseService.test.ts
+++ __tests__/courseService.test.ts
@@ -39,251 +39,261 @@ beforeAll(async () => {
   )
 })
 
-test(
-  'updateCourseDatabase',
-  (done) => {
-    mocked(fetchCoursesFromKdbUseCase).mockImplementation(async (_) => testData)
-    mocked(updateCourseDatabaseUseCase).mockImplementation(
-      async (year, courses) => ({
-        insertedCourses: courses
-          .filter((_, i) => i < courses.length / 5)
-          .map((c) => ({
-            id: v4(),
-            code: c.code,
-            name: c.name,
-          })),
-        updatedCourses: courses
-          .filter((_, i) => i >= courses.length / 5)
-          .map((c) => ({
-            id: v4(),
-            code: c.code,
-            name: c.name,
-          })),
-      })
-    )
-    const req = new UpdateCourseDatabaseRequest()
-    req.setYear(2020)
-    client.updateCourseDatabase(req, (err, value) => {
-      expect(err).toBeFalsy()
-      expect(value).toBeTruthy()
-      if (!value) throw new Error()
-      expect(value.getInsertedcoursesList().length).toBe(testData.length / 5)
-      expect(value.getUpdatedcoursesList().length).toBe(
-        (testData.length / 5) * 4
+describe('updateCourseDatabase', () => {
+  test(
+    'Success',
+    (done) => {
+      mocked(fetchCoursesFromKdbUseCase).mockImplementation(
+        async (_) => testData
       )
+      mocked(updateCourseDatabaseUseCase).mockImplementation(
+        async (year, courses) => ({
+          insertedCourses: courses
+            .filter((_, i) => i < courses.length / 5)
+            .map((c) => ({
+              id: v4(),
+              code: c.code,
+              name: c.name,
+            })),
+          updatedCourses: courses
+            .filter((_, i) => i >= courses.length / 5)
+            .map((c) => ({
+              id: v4(),
+              code: c.code,
+              name: c.name,
+            })),
+        })
+      )
+      const req = new UpdateCourseDatabaseRequest()
+      req.setYear(2020)
+      client.updateCourseDatabase(req, (err, value) => {
+        expect(err).toBeFalsy()
+        expect(value).toBeTruthy()
+        if (!value) throw new Error()
+        expect(value.getInsertedcoursesList().length).toBe(testData.length / 5)
+        expect(value.getUpdatedcoursesList().length).toBe(
+          (testData.length / 5) * 4
+        )
 
+        done()
+      })
+    },
+    1000 * 60 * 10
+  )
+
+  test(
+    'NoCoursesFoundError',
+    (done) => {
+      mocked(fetchCoursesFromKdbUseCase).mockImplementation(async (_) => {
+        throw new NoCoursesFoundError()
+      })
+      mocked(updateCourseDatabaseUseCase).mockImplementation(
+        async (year, courses) => ({
+          insertedCourses: [],
+          updatedCourses: [],
+        })
+      )
+      const req = new UpdateCourseDatabaseRequest()
+      req.setYear(2020)
+      client.updateCourseDatabase(req, (err, value) => {
+        expect(err).toBeTruthy()
+        if (!err) throw new Error()
+        expect(err.code).toBe(Status.UNKNOWN)
+        expect(err.details).toBe('Search results are empty')
+        done()
+      })
+    },
+    1000 * 60 * 10
+  )
+})
+
+describe('getCourses', () => {
+  test('Success', async (done) => {
+    const testids = [v4(), v4()]
+    mocked(getCoursesUseCase).mockImplementation(async (ids) => [
+      createDBCourse(testData[0], 2020, ids[0]),
+      createDBCourse(testData[1], 2020, ids[1]),
+    ])
+    const req = new GetCoursesRequest()
+    req.setIdsList(testids)
+    client.getCourses(req, (err, res) => {
+      expect(err).toBeNull()
+      expect(res).toBeTruthy()
+      if (!res) throw new Error()
+      res.getCoursesList().forEach((c, i) => {
+        expect(c.getId()).toBe(testids[i])
+      })
       done()
     })
-  },
-  1000 * 60 * 10
-)
+  })
 
-test(
-  'updateCourseDatabase:NoCoursesFoundError',
-  (done) => {
-    mocked(fetchCoursesFromKdbUseCase).mockImplementation(async (_) => {
-      throw new NoCoursesFoundError()
-    })
-    mocked(updateCourseDatabaseUseCase).mockImplementation(
-      async (year, courses) => ({
-        insertedCourses: [],
-        updatedCourses: [],
-      })
-    )
-    const req = new UpdateCourseDatabaseRequest()
-    req.setYear(2020)
-    client.updateCourseDatabase(req, (err, value) => {
+  test('notfound', async (done) => {
+    const testids = [v4(), v4()]
+    mocked(getCoursesUseCase).mockImplementation(async (ids) => [])
+    const req = new GetCoursesRequest()
+    req.setIdsList(testids)
+    client.getCourses(req, (err, _) => {
       expect(err).toBeTruthy()
       if (!err) throw new Error()
-      expect(err.code).toBe(Status.UNKNOWN)
-      expect(err.details).toBe('Search results are empty')
+      expect(err.code).toBe(Status.NOT_FOUND)
+      expect(typeof err.metadata.get('ids')[0]).toBe('string')
+      expect((err.metadata.get('ids')[0] as string).split(',')).toEqual(testids)
       done()
     })
-  },
-  1000 * 60 * 10
-)
+  })
 
-test('getCourses', async (done) => {
-  const testids = [v4(), v4()]
-  mocked(getCoursesUseCase).mockImplementation(async (ids) => [
-    createDBCourse(testData[0], 2020, ids[0]),
-    createDBCourse(testData[1], 2020, ids[1]),
-  ])
-  const req = new GetCoursesRequest()
-  req.setIdsList(testids)
-  client.getCourses(req, (err, res) => {
-    expect(err).toBeNull()
-    expect(res).toBeTruthy()
-    if (!res) throw new Error()
-    res.getCoursesList().forEach((c, i) => {
-      expect(c.getId()).toBe(testids[i])
+  test('notfound2', async (done) => {
+    const testids = [v4(), v4()]
+    mocked(getCoursesUseCase).mockImplementation(async (ids) => [
+      createDBCourse(testData[0], 2020, testids[0]),
+    ])
+    const req = new GetCoursesRequest()
+    req.setIdsList(testids)
+    client.getCourses(req, (err, _) => {
+      expect(err).toBeTruthy()
+      if (!err) throw new Error()
+      expect(err.code).toBe(Status.NOT_FOUND)
+      expect(typeof err.metadata.get('ids')[0]).toBe('string')
+      expect(err.metadata.get('ids')[0]).toEqual(testids[1])
+      done()
     })
-    done()
   })
-})
 
-test('getCourses:notfound', async (done) => {
-  const testids = [v4(), v4()]
-  mocked(getCoursesUseCase).mockImplementation(async (ids) => [])
-  const req = new GetCoursesRequest()
-  req.setIdsList(testids)
-  client.getCourses(req, (err, _) => {
-    expect(err).toBeTruthy()
-    if (!err) throw new Error()
-    expect(err.code).toBe(Status.NOT_FOUND)
-    expect(typeof err.metadata.get('ids')[0]).toBe('string')
-    expect((err.metadata.get('ids')[0] as string).split(',')).toEqual(testids)
-    done()
-  })
-})
-
-test('getCourses:notfound2', async (done) => {
-  const testids = [v4(), v4()]
-  mocked(getCoursesUseCase).mockImplementation(async (ids) => [
-    createDBCourse(testData[0], 2020, testids[0]),
-  ])
-  const req = new GetCoursesRequest()
-  req.setIdsList(testids)
-  client.getCourses(req, (err, _) => {
-    expect(err).toBeTruthy()
-    if (!err) throw new Error()
-    expect(err.code).toBe(Status.NOT_FOUND)
-    expect(typeof err.metadata.get('ids')[0]).toBe('string')
-    expect(err.metadata.get('ids')[0]).toEqual(testids[1])
-    done()
-  })
-})
-
-test('getCourses:duplicated ids', async (done) => {
-  const testid = v4()
-  mocked(getCoursesUseCase).mockImplementation(async (ids) => [])
-  const req = new GetCoursesRequest()
-  req.setIdsList([testid, testid])
-  client.getCourses(req, (err, _) => {
-    expect(err).toBeTruthy()
-    if (!err) throw new Error()
-    expect(err.code).toBe(Status.INVALID_ARGUMENT)
-    done()
-  })
-})
-
-test('getCoursesByCode', async (done) => {
-  const conditions = [
-    { year: 2020, code: testData[0].code },
-    { year: 2020, code: testData[1].code },
-  ]
-  mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [
-    createDBCourse(testData[0], 2020, v4()),
-    createDBCourse(testData[1], 2020, v4()),
-  ])
-  const req = new GetCoursesByCodeRequest()
-  req.setConditionsList(
-    conditions.map((c) => {
-      const r = new GetCoursesByCodeRequestCondition()
-      r.setYear(c.year)
-      r.setCode(c.code)
-      return r
+  test('duplicated ids', async (done) => {
+    const testid = v4()
+    mocked(getCoursesUseCase).mockImplementation(async (ids) => [])
+    const req = new GetCoursesRequest()
+    req.setIdsList([testid, testid])
+    client.getCourses(req, (err, _) => {
+      expect(err).toBeTruthy()
+      if (!err) throw new Error()
+      expect(err.code).toBe(Status.INVALID_ARGUMENT)
+      done()
     })
-  )
-  client.getCoursesByCode(req, (err, res) => {
-    expect(err).toBeNull()
-    expect(res).toBeTruthy()
-    if (!res) throw new Error()
-    res.getCoursesList().forEach((c, i) => {
-      expect(c.getYear()).toBe(conditions[i].year)
-      expect(c.getCode()).toBe(conditions[i].code)
-    })
-    done()
   })
 })
 
-test('getCoursesByCode:notfound', async (done) => {
-  const conditions = [
-    { year: 2020, code: testData[0].code },
-    { year: 2020, code: testData[1].code },
-  ]
-  mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [])
-  const req = new GetCoursesByCodeRequest()
-  req.setConditionsList(
-    conditions.map((c) => {
-      const r = new GetCoursesByCodeRequestCondition()
-      r.setYear(c.year)
-      r.setCode(c.code)
-      return r
+describe('getCoursesByCode', () => {
+  test('Success', async (done) => {
+    const conditions = [
+      { year: 2020, code: testData[0].code },
+      { year: 2020, code: testData[1].code },
+    ]
+    mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [
+      createDBCourse(testData[0], 2020, v4()),
+      createDBCourse(testData[1], 2020, v4()),
+    ])
+    const req = new GetCoursesByCodeRequest()
+    req.setConditionsList(
+      conditions.map((c) => {
+        const r = new GetCoursesByCodeRequestCondition()
+        r.setYear(c.year)
+        r.setCode(c.code)
+        return r
+      })
+    )
+    client.getCoursesByCode(req, (err, res) => {
+      expect(err).toBeNull()
+      expect(res).toBeTruthy()
+      if (!res) throw new Error()
+      res.getCoursesList().forEach((c, i) => {
+        expect(c.getYear()).toBe(conditions[i].year)
+        expect(c.getCode()).toBe(conditions[i].code)
+      })
+      done()
     })
-  )
-  client.getCoursesByCode(req, (err, _) => {
-    expect(err).toBeTruthy()
-    if (!err) throw new Error()
-    expect(err.code).toBe(Status.NOT_FOUND)
-    expect(typeof err.metadata.get('conditions')[0]).toBe('string')
-    // expect((err.metadata.get('conditions')[0] as string).split(',')).toEqual(testids)
-    done()
+  })
+
+  test('notfound', async (done) => {
+    const conditions = [
+      { year: 2020, code: testData[0].code },
+      { year: 2020, code: testData[1].code },
+    ]
+    mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [])
+    const req = new GetCoursesByCodeRequest()
+    req.setConditionsList(
+      conditions.map((c) => {
+        const r = new GetCoursesByCodeRequestCondition()
+        r.setYear(c.year)
+        r.setCode(c.code)
+        return r
+      })
+    )
+    client.getCoursesByCode(req, (err, _) => {
+      expect(err).toBeTruthy()
+      if (!err) throw new Error()
+      expect(err.code).toBe(Status.NOT_FOUND)
+      expect(typeof err.metadata.get('conditions')[0]).toBe('string')
+      // expect((err.metadata.get('conditions')[0] as string).split(',')).toEqual(testids)
+      done()
+    })
+  })
+
+  test('notfound2', async (done) => {
+    const conditions = [
+      { year: 2020, code: testData[0].code },
+      { year: 2020, code: testData[1].code },
+    ]
+    mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [
+      createDBCourse(testData[0], 2020, v4()),
+    ])
+    const req = new GetCoursesByCodeRequest()
+    req.setConditionsList(
+      conditions.map((c) => {
+        const r = new GetCoursesByCodeRequestCondition()
+        r.setYear(c.year)
+        r.setCode(c.code)
+        return r
+      })
+    )
+    client.getCoursesByCode(req, (err, _) => {
+      expect(err).toBeTruthy()
+      if (!err) throw new Error()
+      expect(err.code).toBe(Status.NOT_FOUND)
+      expect(typeof err.metadata.get('conditions')[0]).toBe('string')
+      // expect(err.metadata.get('conditions')[0]).toEqual(testids[1])
+      done()
+    })
+  })
+
+  test('duplicated conditions', async (done) => {
+    const conditions = [
+      { year: 2020, code: testData[0].code },
+      { year: 2020, code: testData[0].code },
+    ]
+    mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [])
+    const req = new GetCoursesByCodeRequest()
+    req.setConditionsList(
+      conditions.map((c) => {
+        const r = new GetCoursesByCodeRequestCondition()
+        r.setYear(c.year)
+        r.setCode(c.code)
+        return r
+      })
+    )
+    client.getCoursesByCode(req, (err, _) => {
+      expect(err).toBeTruthy()
+      if (!err) throw new Error()
+      expect(err.code).toBe(Status.INVALID_ARGUMENT)
+      done()
+    })
   })
 })
 
-test('getCoursesByCode:notfound2', async (done) => {
-  const conditions = [
-    { year: 2020, code: testData[0].code },
-    { year: 2020, code: testData[1].code },
-  ]
-  mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [
-    createDBCourse(testData[0], 2020, v4()),
-  ])
-  const req = new GetCoursesByCodeRequest()
-  req.setConditionsList(
-    conditions.map((c) => {
-      const r = new GetCoursesByCodeRequestCondition()
-      r.setYear(c.year)
-      r.setCode(c.code)
-      return r
+describe('listAllCourses', () => {
+  test('Success', async (done) => {
+    mocked(listAllCoursesUseCase).mockImplementation(async () =>
+      testData.map((c) => createDBCourse(c, 2020, v4()))
+    )
+    const req = new ListAllCoursesRequest()
+    client.listAllCourses(req, (err, res) => {
+      expect(err).toBeNull()
+      expect(res).toBeTruthy()
+      if (!res) throw new Error()
+
+      expect(res.getCoursesList().length).toBe(testData.length)
+      done()
     })
-  )
-  client.getCoursesByCode(req, (err, _) => {
-    expect(err).toBeTruthy()
-    if (!err) throw new Error()
-    expect(err.code).toBe(Status.NOT_FOUND)
-    expect(typeof err.metadata.get('conditions')[0]).toBe('string')
-    // expect(err.metadata.get('conditions')[0]).toEqual(testids[1])
-    done()
-  })
-})
-
-test('getCoursesByCode:duplicated conditions', async (done) => {
-  const conditions = [
-    { year: 2020, code: testData[0].code },
-    { year: 2020, code: testData[0].code },
-  ]
-  mocked(getCoursesByCodeUseCase).mockImplementation(async (ids) => [])
-  const req = new GetCoursesByCodeRequest()
-  req.setConditionsList(
-    conditions.map((c) => {
-      const r = new GetCoursesByCodeRequestCondition()
-      r.setYear(c.year)
-      r.setCode(c.code)
-      return r
-    })
-  )
-  client.getCoursesByCode(req, (err, _) => {
-    expect(err).toBeTruthy()
-    if (!err) throw new Error()
-    expect(err.code).toBe(Status.INVALID_ARGUMENT)
-    done()
-  })
-})
-
-test('listAllCourses', async (done) => {
-  mocked(listAllCoursesUseCase).mockImplementation(async () =>
-    testData.map((c) => createDBCourse(c, 2020, v4()))
-  )
-  const req = new ListAllCoursesRequest()
-  client.listAllCourses(req, (err, res) => {
-    expect(err).toBeNull()
-    expect(res).toBeTruthy()
-    if (!res) throw new Error()
-
-    expect(res.getCoursesList().length).toBe(testData.length)
-    done()
   })
 })
 
